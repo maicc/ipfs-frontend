66f23c82f525776bc71d8b68d7a736f0f6c5f82a
maic <juanci123z@gmail.com>
2025-11-02 23:13:35 -0500

    añadido json

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path')
 const app = express();
 const port = 1234;
 
+
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res)=>{
